46190c15e8c04930964a9e037c8ddd5102ba3d36
dev_ashishpal <ashishpal438@gmail.com>
2023-05-24 11:24:40 +0530

    update paypal integration

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+import { getProductById, getProducts } from "../controllers/productController.js";
+
+router.route("/").get(getProducts)
+
+router.route("/:id").get(getProductById);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,43 @@
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import connectDB from "./config/db.js";
+import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
+
+app.use(notFound);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5500;
+
+app.listen(
+  PORT,
+  console.log(
+    `server listening in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow
+      .bold
+  )
+);
