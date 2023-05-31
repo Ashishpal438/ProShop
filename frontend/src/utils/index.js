4eb67fb0e4cd2dc69db24bb81fa8b10b74b4a3e7
dev_ashishpal <ashishpal438@gmail.com>
2023-05-31 15:56:11 +0530

    update production url

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -3,9 +3,11 @@ import axios from "axios";
 // const BASE_URL = "http://localhost:5500";
 let BASE_URL;
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
+  BASE_URL = "https://proshop-fics.onrender.com";
+} else {
   BASE_URL = "http://localhost:5500";
-} 
+}
 
 export const Axios = axios.create({
   baseURL: `${BASE_URL}`,
