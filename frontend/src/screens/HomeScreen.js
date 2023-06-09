19e358c3206eaf5645ede54987aca5a6d5ce26e9
dev_ashishpal <ashishpal438@gmail.com>
2023-06-10 04:11:55 +0530

    add: swiper

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,6 +6,78 @@ import { listProducts } from "../actions/productActions";
 import Loader from "../components/Loader";
 import Message from "../components/Message";
 
+import { Pagination, Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+// import "./style.css";
+
+const Banner = () => {
+  const images = [
+    {
+      id: 1,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/4/25/b7eb901e-6886-4d6f-9567-e8869a55485b1682423953407-Desktop-banner-April---23.jpg",
+    },
+    {
+      id: 2,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2023/2/14/7cec9b95-a683-473c-aca8-cc510821b1cd1676394720493-Desktop-Banner.gif",
+    },
+    {
+      id: 3,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/84b6a214-9eb3-49eb-9f9d-72cec56ec5d71659019908592-Indian-Wear_DK--1-.jpg",
+    },
+    {
+      id: 4,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/179e278f-77ee-44c2-bf39-9f00b0cd08e01658752429301-Handbags_Desk.jpg",
+    },
+    {
+      id: 5,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/6107d28b-2bcb-44e6-9743-655b54550b8f1659020199598-Workwear_Desk--1-.jpg",
+    },
+    {
+      id: 6,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/5/31/4031994d-9092-4aa7-aea1-f52f2ae5194f1654006594976-Activewear_DK.jpg",
+    },
+    {
+      id: 7,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/28/0174e4d7-448c-4746-8572-69461ad5be101659020268081-Tops---Tees_Desk.jpg",
+    },
+    {
+      id: 8,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/b656a7f4-4688-4997-bb7c-54b78793981e1658752386588-Western-Wear_Desk.jpg",
+    },
+    {
+      id: 9,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/7/25/9be788ff-39a4-4214-99d0-fc97505aae5a1658752545685-USPA_Desk_Banner.jpg",
+    },
+    {
+      id: 10,
+      url: "https://assets.myntassets.com/f_webp,w_980,c_limit,fl_progressive,dpr_2.0/assets/images/2022/6/27/53b4daed-cd2c-4111-86c5-14f737eceb351656325318973-Handbags_Desk.jpg",
+    },
+  ];
+  return (
+    <Swiper
+      pagination={true}
+      modules={[Pagination, Autoplay]}
+      className="mySwiper"
+      loop={true}
+      speed={300}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+    >
+      {images.map((image) => {
+        return (
+          <SwiperSlide key={image.id}>
+            <img src={image.url} alt="image" />
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
+};
+
 const HomeScreen = () => {
   const dispatch = useDispatch();
 
@@ -19,6 +91,7 @@ const HomeScreen = () => {
 
   return (
     <>
+      <Banner />
       <h1>Latest Products</h1>
       {loading ? (
         <Loader />
